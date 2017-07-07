f4289a75317f7a26251e3fce306558e7eec67249
Mountain <18207481578@163.com>
2017-07-08 00:08:48 +0800

    Finish the code of loadNiiData.py, but have not test it

--- utils/loadNiiData.py
+++ utils/loadNiiData.py
@@ -4,161 +4,80 @@ import theano
 import logging
 import os
 
-def loadSinglePatientData(patientDir, useROI, modals, normType, forTestData = False):
+
+def loadSinglePatientData(patientDir, normType = 'normImage', modals, label = True, ROI = True):
 
     logger = logging.getLogger(__name__)
 
     assert os.path.isdir(patientDir)
 
-    imageNameList, labelNameList = findImgAndLabelFileNameList(patientDir, 
-                                                               useROI, 
-                                                               modals, 
-                                                               normType,
-                                                               forTestData)
+    modalsDict = {'tice':0, 't1':1, 't2':2, 'flair':3}
 
-    imageNameWithPathList = [os.path.join(patientDir, imageName) 
-                             for imageName in imageNameList]
-    
-    imageArrayList = [readImageArray(imageNameWithPath) 
-                      for imageNameWithPath in imageNameWithPathList]
+    imageArrayList = []
+    labelArray = []
+    ROIArray = []
 
-    patientImageArray = np.asarray(imageArrayList, dtype = theano.config.floatX)
-    del imageArrayList[:], imageArrayList
-    assert patientImageArray.shape == (len(modals), 155, 240, 240)
+    modalsPathList = []
+    labelPath = ''
+    ROIPath = ''
 
-    if len(labelNameList) == 0:
-        assert forTestData and not useROI
-        return patientImageArray, []
+    for fileName in os.listdir(patientDir):
 
+        if fileName.startswith(normType):
+            modalsPathList.append(os.join(patientDir, fileName))
+        
+        if 'seg' in fileName:
+            labelPath = os.join(patientDir, fileName)
 
-    labelNameWithPathList = [os.path.join(patientDir, labelName)
-                             for labelName in labelNameList]
+        if 'ROI' in fileName:
+            ROIPath = os.join(patientDir, fileName)
 
-    labelArrayList = [readImageArray(labelNameWithPath) 
-                      for labelNameWithPath in labelNameWithPathList]
+    finalList = [''] * 4
 
-    patientLabelArray = np.asarray(labelArrayList, dtype = 'int32')
-    del labelArrayList[:], labelArrayList
-    assert patientLabelArray.shape == (int(not forTestData) + int(useROI), 155, 240, 240)
+    for filePath in modalsPathList:
 
-    return patientImageArray, patientLabelArray
+        filePathSegList = filePath.split('.')
+        modalSeg = filePathSegList[-3]
+        modalSeg = modalSeg.split('_')[-1]
 
+        if modalSeg not in modals: continue
 
-def findImgAndLabelFileNameList(patientDir, useROI, modals, normType, forTestData):
+        modalFig = modalsDict[modalSeg]
+        finalList[modalFig] = filePath
 
-    logger = logging.getLogger(__name__)
+    for idx, filePath in enumerate(finalList):
 
-    modalsDict = {'T1c':0, 'T1':1, 'T2':2, 'Flair':3}
-
-    imageNameList = []
-    labelNameList = []
-
-    for item in os.listdir(patientDir):
-        if item.endswith('txt'): 
+        if filePath == '':
             continue
 
-        assert item.endswith('.mha'), (item, patientDir)
+        imageArrayList.append(readArray(filePath))
 
-        if normType == 0:
-            prefix  = 'normImage'
-        else:
-            prefix = 'normBrain'
+    imageArray = np.asarray(imageArrayList, dtype = theano.floatX)
 
-        # itemNameSegList looks like 
-        # ['normImageVSD', 'Brain', 'XX', 'O', 'MR_T1', '54591', 'mha']
-        itemNameSegList = item.split('.')
-        assert len(itemNameSegList) > 2
+    assert len(imageArrayList) == len(modals)
+
+    if label:
+        assert labelPath != ''
+        labelArray = readArray(labelPath)
+
+    if ROI:
+        assert ROIPath != ''
+        ROIArray = readArray(ROIPath)
 
 
-        if itemNameSegList[0].startswith(prefix):
-
-            modalName = findModalName(item)
-
-            if modalName in modals:
-                imageNameList.append(item)
-                continue
-
-        if itemNameSegList[-2] == 'ROI' and useROI:
-            assert len(itemNameSegList) == 3
-            # Use labelNameList to store ROI file name if useROI == True.
-            labelNameList.append(item)
-            continue
-
-        if itemNameSegList[-3] == 'OT' and not forTestData:
-            labelNameList.append(item)
+    return imageArray, labelArray, ROIArray
 
 
 
-    assert len(imageNameList) == len(modals), '{} == {}'.format(imageNameList, modals)
-    assert len(labelNameList) == int(not forTestData) + int(useROI), \
-                                '{} == {}'.format(labelNameList, int(not forTestData) + int(useROI))
-
-    # This two lists should contain different item.
-    assert set(imageNameList) & set(labelNameList) == set()
-
-    # Now, we should to reorder the image and label name list.
-    # First, assign its right order number.
-    reorderImageNameList = [[modalsDict[findModalName(fileName)], fileName] 
-                             for fileName in imageNameList]
-    # Reorder it via its right order number assigned at last step.
-    reorderImageNameList.sort(key = lambda pair: pair[0])
-    # Release the assigned number.
-    reorderImageNameList = [pair[1] for pair in reorderImageNameList]
-
-    # For logger.
-    imgShortNameLs = [findModalName(fileName) for fileName in imageNameList]
-    reorderImgShortNameLs = [findModalName(fileName) for fileName in reorderImageNameList]
-    logger.debug('We reorder the imageNameList from {} to {}.'.format(imgShortNameLs, 
-                                                                     reorderImgShortNameLs))
-    imageNameList = reorderImageNameList
-
-    # Now turn to the label name list.
-    if not useROI:
-        assert len(labelNameList) == int(not forTestData)
-    elif not forTestData:
-        # First, we extract the ground truth file name
-        assert len(labelNameList) == 2
-        groundTruthList = [fileName 
-                           for fileName in labelNameList 
-                           if fileName.split('.')[-3] == 'OT']
-        # Second, we extract the ROI file name
-        ROIList = [fileName 
-                   for fileName in labelNameList 
-                   if fileName.split('.')[-2] == 'ROI']
-        assert len(groundTruthList) == 1
-        assert len(ROIList) == 1
-
-        # By this way, we can ensure the ground truth file is at the first place.
-        labelNameList = groundTruthList + ROIList
-
-    return imageNameList, labelNameList
-
-
-def findModalName(fileName):
+def readArray(filePath):
 
     logger = logging.getLogger(__name__)
 
-    modalsDict = {'T1c':0, 'T1':1, 'T2':2, 'Flair':3}
+    assert filePath.endswith('.nii.gz')
 
-    fileNameSegList = fileName.split('.')
-    modalName = fileNameSegList[-3]
-    modalName = modalName[3:]
-
-    assert modalName in modalsDict.keys()
-
-    return modalName
-
-
-def readImageArray(filePath):
-
-    logger = logging.getLogger(__name__)
-
-    # Now, only consider read the mha file.
-    assert filePath.endswith('.mha')
-
-    image = sitk.ReadImage(filePath)
-    imageArray = sitk.GetArrayFromImage(image)
-
-    assert imageArray.shape == (155, 240, 240)
+    image = nib.load(filePath)
+    imageArray = image.get_data()
 
     return imageArray
+
+    
